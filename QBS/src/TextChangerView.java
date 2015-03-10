c5abd746d3cf0fe0a48c63d8049ffc1576695d48
Wilge <ruta.lukasz@gmail.com>
2015-03-10 15:27:24 +0100

    fix

--- QBS/src/TextChangerView.java
+++ QBS/src/TextChangerView.java
@@ -1,14 +1,10 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.JEditorPane;
 import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
 import java.awt.Color;
